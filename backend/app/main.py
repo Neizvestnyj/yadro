7a3bbb19dc77e644c76505e8ea0bd35d3f26cd47
Neizvestnyj <pikromat1995@gmail.com>
2025-05-25 00:07:56 +0300

    fix ruff

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,5 @@
-from collections.abc import AsyncGenerator
+from collections.abc import AsyncGenerator, Awaitable, Callable
 from contextlib import asynccontextmanager
-from typing import Awaitable, Callable
 
 from fastapi import Depends, FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
